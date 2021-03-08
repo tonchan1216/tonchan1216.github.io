62105a4ea4ab6bec923be82674433ed5c7d111e6
tonchan1216 <cleartone1216@gmail.com>
2021-03-08 23:31:59 +0900

    change message

--- src/blocks/cta.tsx
+++ src/blocks/cta.tsx
@@ -4,21 +4,22 @@ const Cta: React.FC = () => (
   <section className="s-cta">
     <div className="row">
       <div className="column">
-        <h3 className="h2 section-desc">Need a great reliable web hosting?</h3>
+        <h3 className="h2 section-desc">
+          Need to improve <span>PERFORMANCE</span>?
+        </h3>
       </div>
     </div>
 
     <div className="row cta-content">
       <div className="column">
         <p>
-          We highly recommend{" "}
-          <a href="https://www.dreamhost.com/r.cgi?287326">DreamHost</a>.
-          Powerful web and Wordpress hosting. Guaranteed. Starting at $2.95 per
-          month.
+          I can Get advice on the skill market service{" "}
+          <a href="https://coconala.com/users/2309570">coconala</a>. Please feel
+          free to contact me.
         </p>
 
         <a
-          href="https://www.dreamhost.com/r.cgi?287326"
+          href="https://coconala.com/services/1593368?ref=profile_top_service"
           className="btn btn--primary h-full-width"
         >
           Get Started
