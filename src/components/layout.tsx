f5dbbb38cbedfcbf8bba07a9b869ecba15070fa0
tonchan1216 <cleartone1216@gmail.com>
2021-03-13 17:00:23 +0900

    update css

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,16 +1,7 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React, { ReactNode } from "react"
-// import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Footer from "./footer"
-// import { GlobalStyles } from "../globalStyles"
 
 interface LayoutProps {
   children?: ReactNode
@@ -19,8 +10,6 @@ interface LayoutProps {
 const Layout: React.FC = ({ children }: LayoutProps) => {
   return (
     <>
-      {/* <GlobalStyles /> */}
-
       <Header />
       <main>{children}</main>
       <Footer />
